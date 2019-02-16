711f96323ae65f0cf9263f555d2f6a6291ebc3f5
Paul Cromey <paul.cromey@bbc.co.uk>
2019-02-16 17:47:21 +0000

    Added UUID validation

--- src/main/java/com/cromey/identity/Application.java
+++ src/main/java/com/cromey/identity/Application.java
@@ -2,7 +2,9 @@ package com.cromey.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +12,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public MethodValidationPostProcessor methodValidationPostProcessor() {
+		return new MethodValidationPostProcessor();
 	}
 
 }
--- src/main/java/com/cromey/identity/controller/UserController.java
+++ src/main/java/com/cromey/identity/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.cromey.identity.controller;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
+import javax.validation.constraints.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
 import com.cromey.identity.model.User;
 import com.cromey.identity.service.UserService;
 
+@Validated
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -30,9 +34,20 @@ public class UserController {
 		this.userService = userService;
 	}
 	
+	@GetMapping
+	public ResponseEntity<List<User>> getAll(){
+		List<User> user = userService.readAll();
+		if(Objects.nonNull(user)) {
+			return ResponseEntity.ok().body(user);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+			
+	}
+	
 	@GetMapping(value="{id}")
-	public ResponseEntity<User> get(@PathVariable UUID id){
-		User user = userService.read(id);
+	public ResponseEntity<User> get(@Pattern(regexp="[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}", message="Invalid User Id") @PathVariable String id){
+		User user = userService.read(UUID.fromString(id));
 		if(Objects.nonNull(user)) {
 			return ResponseEntity.ok().body(user);
 		} else {
