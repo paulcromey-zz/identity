980792801941c0e5b015244adeaaf06446c761a9
Paul Cromey <paul.cromey@bbc.co.uk>
2019-03-17 18:22:33 +0000

    Upgrade

--- src/main/java/com/cromey/identity/Application.java
+++ src/main/java/com/cromey/identity/Application.java
@@ -2,9 +2,7 @@ package com.cromey.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,11 +10,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public MethodValidationPostProcessor methodValidationPostProcessor() {
-		return new MethodValidationPostProcessor();
 	}
 
 }
