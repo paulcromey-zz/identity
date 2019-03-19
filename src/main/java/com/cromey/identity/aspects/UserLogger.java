479d6b4175e496542845eac56168f78532c1a78e
Paul Cromey <paul.cromey@bbc.co.uk>
2019-03-19 18:53:29 +0000

    Add duration around logging

--- src/main/java/com/cromey/identity/aspects/UserLogger.java
+++ src/main/java/com/cromey/identity/aspects/UserLogger.java
@@ -1,7 +1,11 @@
 package com.cromey.identity.aspects;
 
+import java.util.concurrent.TimeUnit;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class UserLogger {
 
+  private static final String EXECUTION = "Execution ";
   private static final String BLANK_SPACE = " ";
   private static final String EXITING = "Exiting ";
   private static final String ENTERING = "Entering ";
@@ -35,6 +40,16 @@ public class UserLogger {
     log(ENTERING, joinPoint.getTarget().getClass().getSimpleName(), joinPoint.getSignature().getName());
 
   }
+  
+  @Around("methods()")
+  public Object measureMethodExecutionTime(ProceedingJoinPoint pjp) throws Throwable {
+      long start = System.nanoTime();
+      Object retval = pjp.proceed();
+      long end = System.nanoTime();
+      long duration = TimeUnit.NANOSECONDS.toMillis(end - start);
+      log(EXECUTION, duration);
+      return retval;
+  }
 
   /**
    * executes after class methods.
@@ -55,6 +70,19 @@ public class UserLogger {
         .append(className)
         .append(BLANK_SPACE)
         .append(methodName);
+    
+    String message = sb.toString();
+    
+    logger.debug(message);
+    
+  }
+  
+  private void log(String state, Long duration) {
+    
+    StringBuilder sb = new StringBuilder()
+        .append(state)
+        .append(duration)
+        .append(" ms");
     
     String message = sb.toString();
     
